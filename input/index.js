327121c0760d14a998249bad0d604af48bfb43d7
Natsuki Hasegawa <oosacker@gmail.com>
2021-05-20 11:05:15 +1200

    wip

--- input/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.scss";
-console.log("Hello webpack!");
-
-const fancyFunc = () => {
-  return [1, 2];
-};
-
-const [a, b] = fancyFunc();
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: { 
-    index: path.resolve(__dirname, "input", "index.js") 
+    index: path.resolve(__dirname, "input", "main.js") 
   },
   output: {
     path: path.resolve(__dirname, "output")
@@ -18,12 +19,17 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "input", "index.html")
-    })
+    }),
+    new VueLoaderPlugin()
   ]
 };
